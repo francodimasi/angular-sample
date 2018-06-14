b6ebc1cfa7857531f6696b9d2ac6321b2af27409
francodimasi <franco.dimasi@gmail.com>
2018-06-14 15:24:40 -0300

    fix canbet

--- src/app/backend.service.ts
+++ src/app/backend.service.ts
@@ -28,13 +28,6 @@ export class BackendService {
         tap(fixture => fixture.forEach(match => {
             this.getPrediction(match.id).subscribe(prediction => {
                 match.prediction = prediction[0];
-                let date1: number = Date.parse(match.matchTime);
-                let date2: number = Date.now();
-                if (((date1 - date2) / 1000 / 60 / 60) + 3 > 2) {
-                  match.canBet = true;
-                } else {
-                  match.canBet = false;
-                }
             })   
           })
         ),
